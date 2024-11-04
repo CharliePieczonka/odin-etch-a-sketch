5a2ca1d5739d5e264d5e8ed2ef1bacced771ad82
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-04 19:53:28 +0000

    Add reset button
    The reset button clear the board and prompts the use to change the
    number of cells in the grid. Grid size remains the same, cells shrink
    or grow to fill the grid.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,55 @@
 let gridContainer = document.querySelector(".gridContainer");
+let reset = document.querySelector("#reset");
 
-// create a 16 x 16 grid of divs
-for(let y = 0; y < 16; y++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    row.id = "row" + y;
-
-    for(let i = 0; i < 16; i++) {
-        let grid = document.createElement("div");
-        grid.classList.add("grid");
-        grid.id = "grid" + i;
-        //grid.textContent = i;
+// button to reset the grid and change the number of squares
+reset.addEventListener("click", () => {
+    let gridNumber = prompt("Please enter the number of grids per side", "16");
+    gridNumber = parseInt(gridNumber);
     
-        row.appendChild(grid);
+    while(isNaN(gridNumber) || gridNumber < 1 || gridNumber > 100) {
+        gridNumber = prompt("Please enter a number between 1 and 100", "16");
+        gridNumber = parseInt(gridNumber);
     }
 
-    gridContainer.appendChild(row);
-}
+    // clear previous grid
+    while(gridContainer.children.length > 0) {
+        gridContainer.lastChild.remove();
+    }
 
-let grids = document.querySelectorAll(".grid");
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "red";
-    });
+    createGrid(gridNumber);
 });
 
+// draws a grid of squares based on the gridNumber input
+function createGrid(gridNumber) {
+    // grid container remains the same, squares adjust in size to fit
+    let size = 256;
+    let gridPixels = size / gridNumber + "px";
 
+    for(let y = 0; y < gridNumber; y++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.id = "row" + y;
 
+        for(let i = 0; i < gridNumber; i++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.id = "grid" + i;
+            grid.style.width = gridPixels;
+            grid.style.height = gridPixels;
+        
+            row.appendChild(grid);
+        }
 
+        gridContainer.appendChild(row);
+    }
+
+    // create etch-a-sketch effect
+    let grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "red";
+        });
+    });
+}
+
+createGrid(16);
