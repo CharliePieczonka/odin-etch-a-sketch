0e79bf2488c8d71bb644705153405583733f8851
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-05 15:34:49 +0000

    Refactor code and add new button
    Made separate buttons for clearing the grid and setting the number of
    squares. Changed some styling, added a header. Renamed some variables
    to be more accurate.

--- javascript.js
+++ javascript.js
@@ -1,55 +1,73 @@
 let gridContainer = document.querySelector(".gridContainer");
-let reset = document.querySelector("#reset");
+let clear = document.querySelector("#clear");
+let resize = document.querySelector("#resize");
 
-// button to reset the grid and change the number of squares
-reset.addEventListener("click", () => {
-    let gridNumber = prompt("Please enter the number of grids per side", "16");
-    gridNumber = parseInt(gridNumber);
+// button to clear the grid but keep it the number of cells
+clear.addEventListener("click", ()=> {
+    // get number of squares per row of current grid
+    let row = document.querySelector("#row0");
+    let numSquares = row.children.length;
     
-    while(isNaN(gridNumber) || gridNumber < 1 || gridNumber > 100) {
-        gridNumber = prompt("Please enter a number between 1 and 100", "16");
-        gridNumber = parseInt(gridNumber);
-    }
-
-    // clear previous grid
-    while(gridContainer.children.length > 0) {
-        gridContainer.lastChild.remove();
-    }
-
-    createGrid(gridNumber);
+    clearGrid();
+    createGrid(numSquares);
 });
 
-// draws a grid of squares based on the gridNumber input
-function createGrid(gridNumber) {
-    // grid container remains the same, squares adjust in size to fit
-    let size = 256;
-    let gridPixels = size / gridNumber + "px";
+// button to change the number of squares in the grid
+resize.addEventListener("click", () => {
+    let numSquares = "";
+    
+    while(isNaN(numSquares) || numSquares < 1 || numSquares > 100) {
+        numSquares = prompt("Please enter the number of squares per side, between 1 and 100", "16");
+        
+        if(numSquares == null) {
+            return;
+        }
 
-    for(let y = 0; y < gridNumber; y++) {
+        numSquares = parseInt(numSquares);
+    }
+
+    clearGrid();
+    createGrid(numSquares);
+});
+
+// draws a grid of squares based on the numSquares input
+function createGrid(numSquares) {
+    // grid container remains the same, squares adjust in size to fit
+    let size = gridContainer.offsetWidth;
+    let squarePixels = size / numSquares + "px";
+
+    for(let y = 0; y < numSquares; y++) {
         let row = document.createElement("div");
         row.classList.add("row");
         row.id = "row" + y;
 
-        for(let i = 0; i < gridNumber; i++) {
-            let grid = document.createElement("div");
-            grid.classList.add("grid");
-            grid.id = "grid" + i;
-            grid.style.width = gridPixels;
-            grid.style.height = gridPixels;
+        for(let i = 0; i < numSquares; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.id = "square" + i;
+            square.style.width = squarePixels;
+            square.style.height = squarePixels;
         
-            row.appendChild(grid);
+            row.appendChild(square);
         }
 
         gridContainer.appendChild(row);
     }
 
     // create etch-a-sketch effect
-    let grids = document.querySelectorAll(".grid");
-    grids.forEach((grid) => {
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "red";
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((squares) => {
+        squares.addEventListener("mouseover", () => {
+            squares.style.backgroundColor = "red";
         });
     });
+}
+
+// removes all squares from the grid
+function clearGrid() {
+    while(gridContainer.children.length > 0) {
+        gridContainer.lastChild.remove();
+    }
 }
 
 createGrid(16);
